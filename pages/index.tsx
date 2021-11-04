c820d5339af79bdf5d53599765be1984e6fabedb
Stephen Bonish <sbonish3535@gmail.com>
2021-11-04 17:02:04 -0400

    hiding new work to deploy mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,29 +42,29 @@ import {
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
 
-function isWalletConnected() {
-  //THIS GETS THE THREE DROPDOWN BUTTON OPTIONS AND THE HANDLE FUNCTIONS ASSOCIATED WITH THEM
-  const { publicKey, wallet, disconnect } = useWallet();
-  //THIS TAKES THE BIG PUBLIC KEY FOR THE WALLET AND TURNS IT INTO A SIMPLE ADDRESS TO SEND TO
-  const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
-  if (useWallet() !== null && base58 !== null) {
-    return true;
-  } else return false;
-}
+// function isWalletConnected() {
+//   //THIS GETS THE THREE DROPDOWN BUTTON OPTIONS AND THE HANDLE FUNCTIONS ASSOCIATED WITH THEM
+//   const { publicKey, wallet, disconnect } = useWallet();
+//   //THIS TAKES THE BIG PUBLIC KEY FOR THE WALLET AND TURNS IT INTO A SIMPLE ADDRESS TO SEND TO
+//   const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
+//   if (useWallet() !== null && base58 !== null) {
+//     return true;
+//   } else return false;
+// }
 
 const Home: NextPage = () => {
   //TODO: NEED A FUNCTION TO CHECK IF THE WALLET IS CONNECTED
-  var isConnected = isWalletConnected();
+  // var isConnected = isWalletConnected();
 
   //TODO: IF(CONNECTED) NEED A FUNCTION TO TO GET AND HASH THE KEY TO A DATA STRUCTURE
-  if (isConnected) {
-    //TODO:
-    //1.HAVE A DATA-STRUCTURE SET UP THAT WILL BE ABLE TO STORE THE:
-    //(a)USERS-PUBLIC KEYS AND STORE WHERE ITS LOCATED USING AN
-    //(b)INDEX/HASH
-    //2. CREATE A HASH-NUMBER WITH A UNIQUE ID, CHECKING TO MAKE SURE THE SLOT IS EMPTY BEFORE INSERTING
-    //3. WILL NEED GETTER AND SETTER METHODS TO HERE TO MOVE THE COLLECTED PUBLIC KEY AND INDEX NUMBER TO THE DATA STRUCTURE
-  }
+  // if (isConnected) {
+  //   //TODO:
+  //   //1.HAVE A DATA-STRUCTURE SET UP THAT WILL BE ABLE TO STORE THE:
+  //   //(a)USERS-PUBLIC KEYS AND STORE WHERE ITS LOCATED USING AN
+  //   //(b)INDEX/HASH
+  //   //2. CREATE A HASH-NUMBER WITH A UNIQUE ID, CHECKING TO MAKE SURE THE SLOT IS EMPTY BEFORE INSERTING
+  //   //3. WILL NEED GETTER AND SETTER METHODS TO HERE TO MOVE THE COLLECTED PUBLIC KEY AND INDEX NUMBER TO THE DATA STRUCTURE
+  // }
 
   const { publicKey, wallet, disconnect } = useWallet();
   const base58 = useMemo(() => publicKey?.toBase58(), [publicKey]);
